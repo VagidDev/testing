c2b7360cf8f0bedd019cc7f5bb4dff85aa221eee
VagidGev <vagidzibluk@gmail.com>
2025-02-13 12:32:15 +0200

    Add file with matrix, wrote programm for reading file and output the result

--- lab3/first.cpp
+++ lab3/first.cpp
@@ -4,19 +4,54 @@
 using namespace std;
 
 int **matrix;
+int side;
 
-void createAndFillArray() {
-    int cols = 0, rows = 0;
-    matrix = new int*[rows];
+int get_number_of_strings(string filename) {
+    ifstream file(filename);
+    
+    int count = 0;
+    string line;
 
-    for (int i = 0; i < rows; ++i) {
-        matrix[i] = new int[cols];
+    while (getline(file, line))
+        if (!line.empty()) {
+            count++;
+        }
+    
+    file.close();
+    return count;
+}
+
+void create_and_fill_array(string filename) {
+    side = get_number_of_strings(filename);
+    matrix = new int*[side];
+
+    for (int i = 0; i < side; ++i) {
+        matrix[i] = new int[side];
     }
+
+    ifstream file(filename);
+
+    for (int i = 0; i < side; i++)
+    {
+        for (int j = 0; j < side; j++)
+        {
+            file >> matrix[i][j];
+        }
+    }
+    
 }
 
 int main() 
 {
-    
+    string line;
+    cout<<"Input the file name: ";
+    cin>>line;
+    create_and_fill_array(line);
+
+    for (int i = 0; i < side; ++i, cout<<endl) 
+        for (int j = 0; j < side; j++)
+            cout<<matrix[i][j]<<" ";
+        
 
     return  0;    
 } // namespace std;
